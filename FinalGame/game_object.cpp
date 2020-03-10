7bdbd50e24d22c9a90240a03218262552e8eba77
Pedro Arrochela Lobo <40302551+LadyVolk@users.noreply.github.com>
2020-03-10 15:38:47 +0000

    Started final project

---
+++ FinalGame/game_object.cpp
@@ -0,0 +1,33 @@
+#include "game_object.h"
+#include "system/debug_log.h"
+#include <string>
+
+//
+// UpdateFromSimulation
+// 
+// Update the transform of this object from a physics rigid body
+//
+void GameObject::UpdateFromSimulation(const b2Body* body)
+{
+	if (body)
+	{
+		// setup object rotation
+		gef::Matrix44 object_rotation;
+		object_rotation.RotationZ(body->GetAngle());
+
+		// setup the object translation
+		gef::Vector4 object_translation(body->GetPosition().x, body->GetPosition().y, 0.0f);
+
+		// build object transformation matrix
+		gef::Matrix44 object_transform = object_rotation;
+		object_transform.SetTranslation(object_translation);
+		set_transform(object_transform);
+	}
+}
+
+void GameObject::SetType(ObjectType type) {
+	type_member = type;
+}
+GameObject::ObjectType GameObject::GetType() {
+	return type_member;
+}
---
+++ FinalGame/game_object.h
@@ -0,0 +1,22 @@
+#ifndef _GAME_OBJECT_H
+#define _GAME_OBJECT_H
+
+#include <graphics/mesh_instance.h>
+#include <box2d/Box2D.h>
+#include <iostream>
+
+using namespace std;
+
+class GameObject : public gef::MeshInstance
+{
+	
+	
+public:
+	enum ObjectType { player, enemy };
+	ObjectType type_member;
+	void UpdateFromSimulation(const b2Body* body);
+	ObjectType GetType();
+	void SetType(ObjectType type);
+};
+
+#endif // _GAME_OBJECT_H
